c8f60afed8d11ebd6a640a912502e7e3c75fe108
Tyler Garner <garnertb@gmail.com>
2014-01-17 10:20:33 -0500

    Add the chef-solo files.

---
+++ solo/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd /opt/rogue-chef-repo
+source /home/rogue/.rvm/scripts/rvm
+type rvm | head -1
+git pull
+berks update
+berks install --path /opt/chef-run/cookbooks
+rvmsudo chef-solo -c /opt/chef-run/solo.rb -j /opt/chef-run/dna.json
