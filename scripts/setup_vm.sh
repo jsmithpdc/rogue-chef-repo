ac8005c85e4f315100576df54e1e07c168bb716c
Johnathan Garrett <johnathan.garrett@lmnsolutions.com>
2014-07-10 16:54:17 -0400

    Updated setup script to take a vagrant parameter.

--- scripts/setup_vm.sh
+++ scripts/setup_vm.sh
@@ -27,33 +27,40 @@ cd /opt
 git clone https://github.com/ROGUE-JCTD/rogue-chef-repo.git
 
 # Setup Chef Run folder
-
-
-
-#################################################
-
 # if dna.json is in /opt/chef-run, move it out, then run the following, then put it back
 
-################################################
 if [ -f chef-run/dna.json ];
 then
+echo "Copying existing dna.json"
 cp chef-run/dna.json ./dna-copy.json
 cp -r /opt/rogue-chef-repo/solo/* chef-run/
 cp dna-copy.json chef-run/dna.json
 rm dna-copy.json
 cd chef-run
 else
+echo "Using default dna.json"
 mkdir chef-run
 cp -r /opt/rogue-chef-repo/solo/* chef-run/
 cd chef-run
-fi
-
 # Edit dna.json to use correct FQDN… Note: update the url to your server’s url’. If there is no fully qualified domain name, you can simply remove the line from the dna file
 sed -i 's/fqdn/fqdn-ignore/g' dna.json
+# if vagrant is specified, add "vagrant":true
+if test $1 = "vagrant"
+then
+echo "Vagrant specified..."
+sed -i '2 i\
+\  "vagrant": true,
+' dna.json
+fi
+fi
 
 # Change username referenced in provision.sh to correct user if the user on the box is not ‘rogue’ Note: manually view provision.sh and change the user to rogue
 
 chmod 755 *.sh
 
 # to install latest rogue run this .sh file. if you remove the /var/lib/geoserver_data folder, it will download it again. To keep your data, just leave the folder as is and the script will not pull down the basic data folder
+./provision.sh
+
+sudo dpkg -i --force-overwrite /var/cache/apt/archives/libproj-dev_4.8.0-2+opengeo_amd64.deb
+
 ./provision.sh
