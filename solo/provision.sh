999ebe657ecec0cf6ec56dd9c5c81c2d082cbdf2
Syrus <syrus.mesdaghi@lmnsolutions.com>
2014-07-18 11:11:37 -0400

    removing rvmsudo invocation

--- solo/provision.sh
+++ solo/provision.sh
@@ -9,8 +9,10 @@ rm -rf /opt/chef-run/cookbooks
 berks vendor /opt/chef-run/cookbooks
 
 # Note: cannot be in the /opt/rogue-chef-repo when running this command.
-#       ruby shadow install failes!
+#       ruby shadow install failes because chef failes to find some 
+#       packages such as pg and shadow
 cd /opt/chef-run
-rvmsudo chef-solo -c /opt/chef-run/solo.rb -j /opt/chef-run/dna.json
+chef-solo -c /opt/chef-run/solo.rb -j /opt/chef-run/dna.json
+
 # restore stored directory
 popd
