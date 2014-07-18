092dafda272fa53ff28d4299d181b56d7bf8ecd5
Syrus <syrus.mesdaghi@lmnsolutions.com>
2014-07-18 11:02:15 -0400

    workaround for shadow install failure

--- solo/provision.sh
+++ solo/provision.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
-# exit if anything returns failure
-set -e
+#!/usr/bin/env bash
+# store current directory
+pushd .
 
 cd /opt/rogue-chef-repo
-
-# activate the correct rvm environment
-source /usr/local/rvm/scripts/rvm
-
-type rvm | head -1
 git pull
 berks update
 rm -rf /opt/chef-run/cookbooks
 berks vendor /opt/chef-run/cookbooks
+
+# Note: cannot be in the /opt/rogue-chef-repo when running this command.
+#       ruby shadow install failes!
+cd /opt/chef-run
 rvmsudo chef-solo -c /opt/chef-run/solo.rb -j /opt/chef-run/dna.json
+# restore stored directory
+popd
